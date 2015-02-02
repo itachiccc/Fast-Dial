721ea30dee4b945c4c0737563c81c62f3a140e29
Itachi <itachiccc@gmail.com>
2015-02-02 13:51:51 +0800

    Optimization code..

--- src/com/style/fuckdial/MainActivity.java
+++ src/com/style/fuckdial/MainActivity.java
@@ -4,19 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
-
+	private EditText et_number;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn_dial = (Button) findViewById(R.id.btn_dial);
         Button btn_exit = (Button) findViewById(R.id.btn_exit);
+        et_number = (EditText) findViewById(R.id.et_number);
         btn_dial.setOnClickListener(new Call());
         btn_exit.setOnClickListener(new Call());
     }
@@ -27,8 +31,11 @@ public class MainActivity extends Activity {
 		public void onClick(View v) {
 			switch (v.getId()) {
 			case R.id.btn_dial:
-				EditText et_number = (EditText) findViewById(R.id.et_number);
-				String number = et_number.getText().toString();
+				String number = et_number.getText().toString().trim();
+				if (TextUtils.isEmpty(number)) {
+					Toast.makeText(MainActivity.this, "ºÅÂë²»ÄÜÎª¿Õ", Toast.LENGTH_LONG).show();
+					return;
+				}
 				Intent intent = new Intent();
 				intent.setAction(Intent.ACTION_CALL);
 				intent.setData(Uri.parse("tel:"+number));
