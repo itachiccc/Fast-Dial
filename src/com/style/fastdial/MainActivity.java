76e174eef9b1b775c4765137be6572f4118a1d58
Itachi <itachiccc@gmail.com>
2015-02-05 09:47:58 +0800

    optimization code

--- src/com/style/fastdial/MainActivity.java
+++ src/com/style/fastdial/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	private EditText et_number;
 	
     @Override
@@ -22,31 +22,27 @@ public class MainActivity extends Activity {
         Button btn_dial = (Button) findViewById(R.id.btn_dial);
         Button btn_exit = (Button) findViewById(R.id.btn_exit);
         et_number = (EditText) findViewById(R.id.et_number);
-        btn_dial.setOnClickListener(new Call());
-        btn_exit.setOnClickListener(new Call());
+        btn_dial.setOnClickListener(this);
+        btn_exit.setOnClickListener(this);
     }
 
-    private class Call implements OnClickListener {
-
-		@Override
-		public void onClick(View v) {
-			switch (v.getId()) {
-			case R.id.btn_dial:
-				String number = et_number.getText().toString().trim();
-				if (TextUtils.isEmpty(number)) {
-					Toast.makeText(MainActivity.this, "号码不能为空", Toast.LENGTH_LONG).show();
-					return;
-				}
-				Intent intent = new Intent();
-				intent.setAction(Intent.ACTION_CALL);
-				intent.setData(Uri.parse("tel:"+number));
-				startActivity(intent);
-				break;
-			case R.id.btn_exit:
-				android.os.Process.killProcess(android.os.Process.myPid());
-			default:
-				break;
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_dial:
+			String number = et_number.getText().toString().trim();
+			if (TextUtils.isEmpty(number)) {
+				Toast.makeText(MainActivity.this, "号码不能为空", Toast.LENGTH_LONG).show();
+				return;
 			}
+			Intent intent = new Intent();
+			intent.setAction(Intent.ACTION_CALL);
+			intent.setData(Uri.parse("tel:"+number));
+			startActivity(intent);
+			break;
+		case R.id.btn_exit:
+			android.os.Process.killProcess(android.os.Process.myPid());
+			break;
 		}
-    }
+	}
 }
